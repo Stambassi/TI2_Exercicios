770e87530276cef127feb7f1389a4664fc0e7a17
Stambassi <augustostambassiduarte@gmail.com>
2024-08-19 20:55:51 -0300

    Exercicio 1 Completo

--- Exercicio_1/src/myClass/SomarNumero.java
+++ Exercicio_1/src/myClass/SomarNumero.java
@@ -3,5 +3,21 @@ package myClass;
 import java.util.*;
 
 class SomarNumero {
-
+	public static Scanner sc = new Scanner(System.in);
+	public static void main (String args[]) {
+		// Variaveis
+		int num1,num2,soma;
+		
+		// Leituras
+		System.out.print("Digite um numero: ");
+		num1 = sc.nextInt();
+		System.out.print("Digite mais um numero: ");
+		num2 = sc.nextInt();
+		
+		// Soma
+		soma = num1 + num2;
+		
+		// Saida
+		System.out.println("A soma de "+ num1 +" + "+ num2 +" = "+ soma);
+	}
 }
